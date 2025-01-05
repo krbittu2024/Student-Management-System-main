d692ab870ce93033c8e5ac1c10a601c380a79214
krbittu2024 <bittuk85btechcse2024@kccitm.edu.in>
2025-01-06 02:33:10 +0530

    first commit

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -17,7 +17,7 @@ const Homepage = () => {
                         <StyledTitle>
                             Welcome to
                             <br />
-                            School Management
+                            Student Management
                             <br />
                             System
                         </StyledTitle>
